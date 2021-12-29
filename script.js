aabf352f955239949d4c76cd4615c17f255e5e20
Timespike <50344633+Timespike@users.noreply.github.com>
2021-12-28 18:37:29 -0600

    finished play/pause function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
@@ -9,12 +12,19 @@ let isPlaying = false;
 // Play
 function playSong() {
     isPlaying = true;
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', 'Pause')
     music.play();
 }
 
 // Pause
 function pauseSong() {
     isPlaying = false;
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play')
     music.pause();
 }
+
+// Play or Pause Event Listener
+playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
 
